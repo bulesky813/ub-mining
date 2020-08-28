9a76b273e25837df0f796fd602ed0ed417ecd3d2
bulesky813 <421528826@qq.com>
2020-08-28 10:39:53 +0800

    新增用户仓位列表 新增用户静态收益列表

--- app/Request/User/UserChangeAssetsRequest.php
+++ app/Request/User/UserChangeAssetsRequest.php
@@ -6,12 +6,33 @@ namespace App\Request\User;
 
 use App\Request\AbstractRequest;
 use App\Services\Separate\SeparateWarehouseService;
+use App\Services\User\UserWarehouseRecordService;
 use App\Services\User\UserWarehouseService;
 use Hyperf\Validation\Request\FormRequest;
 use Hyperf\Validation\Rule;
+use Hyperf\Di\Annotation\Inject;
 
 class UserChangeAssetsRequest extends AbstractRequest
 {
+
+    /**
+     * @Inject
+     * @var UserWarehouseRecordService
+     */
+    protected $uwrs;
+
+    /**
+     * @Inject
+     * @var UserWarehouseService
+     */
+    protected $uws;
+
+    /**
+     * @Inject
+     * @var SeparateWarehouseService
+     */
+    protected $sws;
+
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -54,13 +75,11 @@ class UserChangeAssetsRequest extends AbstractRequest
                     $user_id = (int)$this->input('user_id');
                     $coin_symbol = (string)$this->input('coin_symbol');
                     $separate_warehouse_sort = (int)$this->input('separate_warehouse_sort');
-                    $uw = new UserWarehouseService();
-                    $user_warehouse = $uw->userWarehouse($user_id, $coin_symbol); //获取用户所有持仓
+                    $user_warehouse = $this->uws->userWarehouse($user_id, $coin_symbol); //获取用户所有持仓
                     if ($user_warehouse->isEmpty() && $separate_warehouse_sort > 1) {
                         return $fail('只能从1号仓开始加仓!');
                     }
-                    $sws = new SeparateWarehouseService();
-                    $symbol_separate_warehouse = $sws->separateWarehouse(
+                    $symbol_separate_warehouse = $this->sws->separateWarehouse(
                         $coin_symbol,
                         $separate_warehouse_sort
                     )->offsetGet(0);//获取当前操作仓位的详细
@@ -79,6 +98,10 @@ class UserChangeAssetsRequest extends AbstractRequest
                             return $fail(sprintf("该仓位最大持仓量必须小于或等于 %s", $symbol_separate_warehouse->high));
                         }
                     } else {
+                        $today_revoke_record = $this->uwrs->todayRevoke($user_id);
+                        if ($today_revoke_record) {
+                            return $fail('每日只能撤仓一次');
+                        }
                         if ($separate_warehouse_sort < $user_warehouse->count()) {
                             return $fail(sprintf('必须从%d号仓位开始撤仓', $user_warehouse->count()));
                         }
--- app/Services/User/UserWarehouseRecordService.php
+++ app/Services/User/UserWarehouseRecordService.php
@@ -3,7 +3,9 @@
 namespace App\Services\User;
 
 use App\Services\AbstractService;
+use Carbon\Carbon;
 use Hyperf\Database\Model\Model;
+use function Zipkin\Timestamp\now;
 
 class UserWarehouseRecordService extends AbstractService
 {
@@ -13,5 +15,30 @@ class UserWarehouseRecordService extends AbstractService
     {
         $user_warehouse_record = $this->create($attr);
         return $user_warehouse_record;
+    }
+
+    public function todayRevoke(int $user_id)
+    {
+        return $this->get([
+            'user_id' => $user_id,
+            'num' => [
+                'condition' => 'function',
+                'data' => function ($query) {
+                    $query->where('num', '<', 0);
+                }
+            ],
+            'created_at' => [
+                'condition' => 'function',
+                'data' => function ($query) {
+                    $query->whereBetween(
+                        'created_at',
+                        [
+                            Carbon::now()->startOfDay()->toDateTimeString(),
+                            Carbon::now()->toDateTimeString()
+                        ]
+                    );
+                }
+            ]
+        ]);
     }
 }
