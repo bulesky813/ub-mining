e3d57991fb9a8b3f8ab4afb15c0bdb5f9cafaa87
bulesky813 <421528826@qq.com>
2020-09-04 10:34:01 +0800

    精度取两位小数

--- app/Job/PullOutJob.php
+++ app/Job/PullOutJob.php
@@ -9,9 +9,11 @@ use App\Services\Mine\SeparateWarehouseService;
 use App\Services\User\UserAssetsService;
 use App\Services\User\UserRelationService;
 use App\Services\User\UsersService;
+use App\Services\User\UserWarehouseRecordService;
 use App\Services\User\UserWarehouseService;
 use Hyperf\AsyncQueue\Job;
 use Hyperf\Database\Model\Collection;
+use Hyperf\DbConnection\Db;
 use Hyperf\Utils\Arr;
 use Hyperf\Utils\Exception\ParallelExecutionException;
 use Hyperf\Utils\Parallel;
@@ -45,6 +47,12 @@ class PullOutJob extends Job
      */
     protected $uws;
 
+    /**
+     * @Inject
+     * @var UserWarehouseRecordService
+     */
+    protected $uwrs;
+
     public function __construct($params)
     {
         $this->params = $params;
@@ -66,14 +74,50 @@ class PullOutJob extends Job
             $parallel->add(function () use ($user_warehouse) {
                 $timestamp = (string)time();
                 $token = hash_hmac('sha256', $timestamp, config('mining.app_secret_key'));
-                $this->hs->lessFreeze([
+                $data = $this->hs->lessFreeze([
                     'uid' => $user_warehouse,
                     'value' => $user_warehouse->assets,
                     'coin_symbol' => $user_warehouse->coin_symbol,
                     'time' => $timestamp,
-                    'token'
+                    'token' => $token
                 ]);
+                if (!$data) {
+                    return;
+                }
+                Db::beginTransaction();
+                try {
+                    $pullout_value = Arr::get($data, 'data.value', 0);
+                    if (bccomp($pullout_value, $user_warehouse->assets) == 0) {
+                        $user_warehouse->assets = 0;
+                        $user_warehouse->save();
+                    } else {
+                        echo sprintf(
+                            "user_id: %d, coin_symbol: %s, sort: %d, pullout: fail",
+                            $user_warehouse->user_id,
+                            $user_warehouse->coin_symbol,
+                            $user_warehouse->sort
+                        ) . PHP_EOL;
+                        return;
+                    }
+                    $this->uwrs->record([
+                        'user_id' => $user_warehouse->user_id,
+                        'coin_symbol' => $user_warehouse->coin_symbol,
+                        'sort' => $user_warehouse->sort,
+                        'value_before' => $user_warehouse->assets,
+                        'num' => bcmul($user_warehouse->assets, '-1'),
+                        'pullout' => 2
+                    ]);
+                    Db::commit();
+                } catch (\Throwable $e) {
+                    Db::rollBack();
+                    echo $e->getMessage() . PHP_EOL;
+                }
             });
         };
+        try {
+            $parallel->wait();
+        } catch (ParallelExecutionException $e) {
+            throw $e;
+        }
     }
 }
